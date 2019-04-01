560e7a27f10a165cf4a51f8a0248dea6098e51e6
quanle51297 <quanle51297@gmail.com>
2019-04-02 01:01:40 +0700

    up

---
+++ public/site/include/connect.php
@@ -0,0 +1,8 @@
+<?php
+//Ket noi csdl
+$o = new PDO("mysql:host=localhost; dbname=dbsago", "root", "");
+//Hien thi tieng viet co dau
+$o->query("set names 'utf8' ");
+//khoi tao session
+if (!isset($_SESSION)) session_start();
+if (!isset($_SESSION['giohang'])) $_SESSION['giohang'] = array();
